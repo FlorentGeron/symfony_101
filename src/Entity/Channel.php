e25ab18c675e1d9232451b18563f8dcdf160dc2a
Florent Geron <florent.geron@orange.fr>
2022-06-03 17:04:59 +0200

    local postgre in the end

---
+++ src/Entity/Channel.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChannelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChannelRepository::class)]
+class Channel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Title;
+
+    #[ORM\OneToMany(mappedBy: 'Channel_id', targetEntity: Message::class, orphanRemoval: true)]
+    private $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setChannelId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getChannelId() === $this) {
+                $message->setChannelId(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Message.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Content;
+
+    #[ORM\Column(type: 'datetime')]
+    private $Created_at;
+
+    #[ORM\ManyToOne(targetEntity: Channel::class, inversedBy: 'messages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $Channel_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): self
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->Created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $Created_at): self
+    {
+        $this->Created_at = $Created_at;
+
+        return $this;
+    }
+
+    public function getChannelId(): ?Channel
+    {
+        return $this->Channel_id;
+    }
+
+    public function setChannelId(?Channel $Channel_id): self
+    {
+        $this->Channel_id = $Channel_id;
+
+        return $this;
+    }
+}
