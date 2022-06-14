e5fdb6d8f070d15ca771ddd1c331d3230e7791db
Florent Geron <florent.geron@orange.fr>
2022-06-14 16:39:40 +0200

    nickname as to_string for users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -108,6 +108,11 @@ class User
         return $this;
     }
 
+    public function __toString()
+    {
+      return $this->Nickname;
+    }
+
     /**
      * @return Collection<int, Channel>
      */
