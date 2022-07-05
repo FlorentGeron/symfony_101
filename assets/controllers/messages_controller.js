2d15c59a2dba4975ad1c1ad7db4d94672f373e50
Florent Geron <florent.geron@orange.fr>
2022-07-05 15:05:03 +0200

    stimulus controller

---
+++ assets/controllers/messages_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ["messageField"]
+
+
+  connect() {
+      console.log( 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js');
+  }
+
+  displaymessage() {
+    this.messageFieldTarget.append("<div>Prout</div>");
+  }
+}
